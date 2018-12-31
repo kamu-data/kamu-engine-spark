cae24e7ce716f7445ff6ec440561b9c2394eb888
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2018-12-30 17:23:32 -0800

    Cities by-name dataset

--- src/main/scala/TransformTask.scala
+++ src/main/scala/TransformTask.scala
@@ -17,19 +17,6 @@ class TransformTask(
     transform.outputs.foreach(registerOutput)
   }
 
-  private def getQueryName(transform: TransformConfig): String = {
-    s"${transform.inputs(0).id} -> ${transform.outputs(0).id}"
-  }
-
-  private def getInputPath(input: InputConfig): Path = {
-    input.kind match {
-      case "root" =>
-        config.dataRootDir.resolve(input.id)
-      case "derivative" =>
-        config.dataDerivativeDir.resolve(input.id)
-    }
-  }
-
   private def registerInputView(input: InputConfig): Unit = {
     logger.info(s"Registering input: ${input.id}")
 
@@ -52,12 +39,25 @@ class TransformTask(
     val outputStream = spark.sql(s"SELECT * FROM `${output.id}`")
 
     outputStream.writeStream
-      .queryName(getQueryName(transform))
+      .queryName(getQueryName(output))
       .trigger(Trigger.ProcessingTime(0))
       .outputMode(OutputMode.Append())
       .option("path", config.dataDerivativeDir.resolve(output.id).toString)
       .option("checkpointLocation", config.checkpointDir.resolve(output.id).toString)
       .format("parquet")
       .start()
+  }
+
+  private def getInputPath(input: InputConfig): Path = {
+    input.kind match {
+      case "root" =>
+        config.dataRootDir.resolve(input.id)
+      case "derivative" =>
+        config.dataDerivativeDir.resolve(input.id)
+    }
+  }
+
+  private def getQueryName(output: OutputConfig): String = {
+    s"{${transform.inputs.mkString(", ")}} -> ${output.id}"
   }
 }
