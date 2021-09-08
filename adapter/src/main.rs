67fb7d6fb9fe1b857a62fcd147b9898e5da4c50d
Sergii Mikhtoniuk <mikhtoniuk@gmail.com>
2021-09-08 15:46:04 -0700

    Integrate tracing

--- adapter/src/main.rs
+++ adapter/src/main.rs
@@ -26,12 +26,24 @@ impl Adapter for AdapterImpl {
     }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
+const BINARY_NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const DEFAULT_LOGGING_CONFIG: &str = "info";
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logging();
+
+    tracing::info!("kamu-engine-spark ODF adapter (v{})", VERSION);
+
     let addr = "0.0.0.0:2884".parse()?;
     let adapter = AdapterImpl::default();
 
-    println!("RUUUUUUUN");
+    tracing::info!("gRPC listening on: http://{}", addr);
 
     Server::builder()
         .add_service(AdapterServer::new(adapter))
@@ -39,4 +51,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     Ok(())
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+fn init_logging() {
+    use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+    use tracing_log::LogTracer;
+    use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+    // Redirect all standard logging to tracing events
+    LogTracer::init().expect("Failed to set LogTracer");
+
+    // Use configuration from RUST_LOG env var if provided
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or(EnvFilter::new(DEFAULT_LOGGING_CONFIG.to_owned()));
+
+    // TODO: Use non-blocking writer?
+    // Configure Bunyan JSON formatter
+    let formatting_layer = BunyanFormattingLayer::new(BINARY_NAME.to_owned(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
 }
